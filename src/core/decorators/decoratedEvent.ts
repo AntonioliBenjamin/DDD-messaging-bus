7d70bbf5427c84bae61c041f0fdc3053951c6662
benjamin antonioli <111560667+AntonioliBenjamin@users.noreply.github.com>
2023-02-02 15:15:31 +0100

    Update decoratedEvent.ts

--- src/core/decorators/decoratedEvent.ts
+++ src/core/decorators/decoratedEvent.ts
@@ -1,11 +1,5 @@
 export function decoratedEvent(value: string) {
-    // this is the decorator factory, it sets up
-    // the returned decorator function
     return function (target) {
         target.eventName = value
-        // console.log(target.name.eventName)
-        // console.log(target)
-        // this is the decorator
-        // do something with 'target' and 'value'...
     };
 }
